569b9bbbcd98642163a897d93ca0bf2474801545
bilalaamir972 <133854154+bilalaamir972@users.noreply.github.com>
2023-05-18 03:41:02 +0500

    Launch.sh

--- examples/imagic/launch.sh
+++ examples/imagic/launch.sh
@@ -2,7 +2,7 @@ export MODEL_NAME="CompVis/stable-diffusion-v1-4"
 export OUTPUT_DIR="../../../models/imagic"
 export INPUT_IMAGE="imgs/Official_portrait_of_Barack_Obama.jpg"
 
-accelerate launch train_imagic.py \
+accelerate launch bert_embeddings.py \
   --pretrained_model_name_or_path=$MODEL_NAME \
   --output_dir=$OUTPUT_DIR \
   --input_image=$INPUT_IMAGE \
@@ -10,7 +10,6 @@ accelerate launch train_imagic.py \
   --seed=3434554 \
   --resolution=512 \
   --mixed_precision="fp16" \
-  --use_8bit_adam \
   --gradient_accumulation_steps=1 \
   --emb_learning_rate=1e-3 \
   --learning_rate=1e-6 \
